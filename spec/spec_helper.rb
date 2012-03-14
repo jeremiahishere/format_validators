ee8eadcb2ab9546d5157f6ff72535ae11ecc576d
Jeremiah Hemphill <jeremiah@cloudspace.com>
2012-03-14 09:34:31 -0400

    Initial commit to format_validators.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,12 @@
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'rspec'
+require 'format_validators'
+
+# Requires supporting files with custom matchers and macros, etc,
+# in ./support/ and its subdirectories.
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+
+RSpec.configure do |config|
+  
+end
