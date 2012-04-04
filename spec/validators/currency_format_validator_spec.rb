15d7f8dfad36d98fce5c1005b7c1249439cae749
Ethan Pemble <epemble@gmail.com>
2012-04-04 10:04:53 -0500

    Included an option :strict

--- spec/validators/currency_format_validator_spec.rb
+++ spec/validators/currency_format_validator_spec.rb
@@ -31,4 +31,31 @@ describe CurrencyFormatValidator do
     it { subject.validate_each @mock, 'date', '23.45.6' }
     it { subject.validate_each @mock, 'date', '.234' }
   end
+  
+  context 'in strict mode' do
+    before :each do
+      @validator = CurrencyFormatValidator.new attributes: {}, strict: true
+    end
+    subject { @validator }
+    context 'with perfectly valid, correctly-formatted input should produce no errors' do
+      before { @mock.should_not_receive :errors }
+      it { subject.validate_each @mock, 'date', '123.45' }
+      it { subject.validate_each @mock, 'date', '23.45' }
+      it { subject.validate_each @mock, 'date', '0.45' }
+      it { subject.validate_each @mock, 'date', '4' }
+    end
+
+    context 'with invalid input should produce a validation error' do
+      before { @mock.errors[].should_receive :<< }
+      it { subject.validate_each @mock, 'date', '.45' }
+      it { subject.validate_each @mock, 'date', '.4' }
+      it { subject.validate_each @mock, 'date', '0.4' }
+      it { subject.validate_each @mock, 'date', '23.456' }
+      it { subject.validate_each @mock, 'date', 'two' }
+      it { subject.validate_each @mock, 'date', '23..456' }
+      it { subject.validate_each @mock, 'date', '23.45.6' }
+      it { subject.validate_each @mock, 'date', '.234' }
+    end
+  end
+
 end
